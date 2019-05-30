5fed59d618ae323d989b6bc0a3797d0fc7b58c15
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-29 17:58:23 -0700

    Update csv_parser.py
    added comments/input to include .tsv in the parsing

--- csv_parser.py
+++ csv_parser.py
@@ -1,9 +1,9 @@
-#csv_parser.py: Simple CSV parser
+#csv_parser.py: Simple CSV/TSV parser
 
 import csv
 
-#user input to open .csv file and print to console
-with open(input('.csv file name: '),'r') as csvfile:
+#user input to open .csv/.tsv file and print to console
+with open(input('.csv/.tsv file name: '),'r') as csvfile:
   read_data = csv.reader(csvfile)
   for row in read_data:
     print(row)
