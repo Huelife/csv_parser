c60f4d0254c2f38ac2f7604b88cddb6ae72da4c3
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-29 17:39:38 -0700

    Update csv_parser.py
    added comment to with open and name to input string

--- csv_parser.py
+++ csv_parser.py
@@ -2,7 +2,8 @@
 
 import csv
 
-with open(input('.csv file: '),'r') as csvfile:
+#user input to open .csv file and print to console
+with open(input('.csv file name: '),'r') as csvfile:
   read_data = csv.reader(csvfile)
   for row in read_data:
     print(row)
