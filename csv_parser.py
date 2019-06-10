2c23d5e2c2babe2332af6cdd44221b3aa691d97e
Eric Meas <emmeas@gmail.com>
2019-06-10 01:22:04 -0700

    Update csv_parser.py
    removed extra ) for output code near as fout and added more space for one of the comments for clarity

--- csv_parser.py
+++ csv_parser.py
@@ -3,9 +3,9 @@
 import csv
 
 #user input to open .csv/.tsv file and print to console
-with open(input('Input: '),'r') as fin,open(input('Output: '),'w')) as fout:
+with open(input('Input: '),'r') as fin,open(input('Output: '),'w') as fout:
   read_data = csv.reader(fin)
   write_data = csv.writer(fout)
   for row in read_data:
-    print(row) #remove print command for quicker parsing
+    print(row)               #remove print command for quicker parsing
     write_data.writerow(row)
